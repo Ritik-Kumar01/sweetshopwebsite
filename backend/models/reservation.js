c41836b149fa75b7718d4822e39e598fe7045413
Ritik <ritikpareek81077@gmail.com>
2024-04-26 00:13:38 +0530

    ji

--- backend/models/reservation.js
+++ backend/models/reservation.js
@@ -33,6 +33,30 @@ const reservationSchema = new mongoose.Schema({
         minLength: [10, "Phone number must contain 10 Digits."],
         maxLength: [10, "Phone number must contain 10 Digits."],
     },
+    sweetname: {
+        type: String,
+        required: true,
+        minLength: [3, "First name must be of at least 3 Characters."],
+        maxLength: [30, "First name cannot exceed 30 Characters."],
+    },
+    qunatity: {
+        type: String,
+        required: true,
+        minLength: [20, "Must be atleast 20 kgs"],
+
+    },
+    street: {
+        type: String,
+        required: true,
+        minLength: [3, "Street name must be of at least 3 Characters."],
+
+    },
+    city: {
+        type: String,
+        required: true,
+        minLength: [3, "City name must be of at least 3 Characters."],
+
+    },
 });
 
 export const Reservation = mongoose.model("Reservation", reservationSchema);
