1d133b86ae14b536204604b15a57b174f0167828
Ritik <ritikpareek81077@gmail.com>
2024-04-25 22:08:23 +0530

    hj

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,36 @@
-
-const PORT=4000;
-
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
-
+const PORT = 4000;
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
+    cors({
+        origin: "https://moonlit-kitsune-e9dd43.netlify.app",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true,
+    })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/reservation", reservationRouter);
+
 app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    success: true,
-    message: "HELLO WORLD AGAIN"
-  })
-})
+    return res.status(200).json({
+        success: true,
+        message: "HELLO WORLD AGAIN"
+    });
+});
 
 dbConnection();
 
 app.use(errorMiddleware);
 
-export default app;
-
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
-})
+});
